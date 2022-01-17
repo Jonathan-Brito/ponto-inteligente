fb97888a8fbbb969abeaf4060ad60dfa69237375
norberto.enomoto@outlook.com <devjonathanbrito@gmail.com>
2022-01-17 15:45:41 -0300

    Commit inicial

---
+++ backend/src/test/java/com/brito/pontointeligente/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brito.pontointeligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
