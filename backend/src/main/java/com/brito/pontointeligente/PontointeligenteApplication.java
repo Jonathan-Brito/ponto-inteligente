dc74116b9bf21eb5893d49109ca6bcfb882f6fb1
norberto.enomoto@outlook.com <devjonathanbrito@gmail.com>
2022-01-20 19:36:11 -0300

    Adicionado metodos para geração de senha com BCrypt

--- backend/src/main/java/com/brito/pontointeligente/PontointeligenteApplication.java
+++ backend/src/main/java/com/brito/pontointeligente/PontointeligenteApplication.java
@@ -1,13 +1,31 @@
 package com.brito.pontointeligente;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.brito.pontointeligente.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class PontointeligenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontointeligenteApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded);
+			
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", 
+					senhaEncoded));
+		};
 	}
 
 }
